9836077c3e121c77604a2b26d75c6faf9a28b033
TimStraube <tileone02@posteo.de>
2025-05-09 00:47:27 +0200

    feat: Added comments for FOC implementation

--- src/main.c
+++ src/main.c
@@ -15,11 +15,47 @@
 
 #define LED_PIN 5
 
+// Back EMF sensing
+#define PIN_INPUT_BACK_EMF_VOLTAGE_1 0
+#define PIN_INPUT_BACK_EMF_VOLTAGE_2 1
+#define PIN_INPUT_BACK_EMF_VOLTAGE_3 2
+
+#define PIN_OUTPUT_PWM_1 3
+#define PIN_OUTPUT_PWM_2 4
+#define PIN_OUTPUT_PWM_3 5
+
+// PWM frequency
+#define PWM_FREQUENCY 20000 // 20 kHz
+
+// Rotor speed reference pin
+#define PIN_INPUT_ROTORSPEED_REF 6
+
 void init_system(void) {
-  
+
 }
 
 void main(void)
 {
+  init_system();
+
+  // ----------------------------------------------
+  // FOC
+  // ----------------------------------------------
+
+  // Estimate the rotor position using Back EMF
+
+  // Estimate the rotor speed
+
+  // Clarke Transform
+
+  // Park Transform
+
+  // PID Controller
+
+  // Inverse Park Transform
+
+  // Space Vector Modulation
+
+  // PWM modulation
 
 }
